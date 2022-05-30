ee3f8b865e90baa8639ce1383650106396d760e1
juliwithoutthee <jnboucree@gmail.com>
2022-05-30 10:46:16 +0900

    Placeholder for friends function

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -2,6 +2,13 @@ import React from "react";
 import Dashboard from "../components/Dashboard";
 import { withProtected } from "../src/hook/route";
 import Navbar from "../components/Navbar";
+import axios from "axios";
+
+async function FindFriends() {
+  const res = await axios.get('/friends'); //edit later
+  console.log(res);
+  
+}
 
 function Profile() {
   return (
